7b40b958785fc8e4261e9d3eb1fcf1d9af9d0633
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2023-01-03 18:18:57 -0600

    Initial commit

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    //target object position
+    [Header("Target Object")]
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+ private void Update()
+    {
+//if the target position on the y axis is greater
+//than the camera position
+if(target.position.y > transform.position.y)
+        {
+            //the camera will follow the targets position
+            transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
