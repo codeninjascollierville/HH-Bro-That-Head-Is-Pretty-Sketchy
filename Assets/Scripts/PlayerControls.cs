4a48ffce43ee9f55a0ec88ed17f9c3d02182539e
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2023-01-17 18:19:32 -0600

    1/17/23

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -5,15 +5,66 @@ using UnityEngine.UI;
 
 public class PlayerControls : MonoBehaviour
 {
+    //rigid body 2d object that is stored
+    [Header("RigidBody")]
+    public Rigidbody2D rb
+        [Header("Default Down Speed")]
+        //downward speed of the object
+        public float downSpeed = 20f
+        [Header("Default Movement Speed")]
+        //movement speed of the object
+        [Header("Default Directional Movement Speed")]
+        //movement direction of the object
+        public float movement = 0f
+
+
+
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
+        //variable equals to component rigidbody 2d
+        rb = GetComponent<Rigidbody2D>();
+
+
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        //movement equals horizontal movement
+        //multiplied by movement speed
+        movement = Input.GetAxis("Horozontal") * movementSpeed;
+        //if direction on x axis is less than 0
+        else
+        {
+            //object faces to the right
+            this.GetComponent<SpriteRenderer>().flipX = true;
+
+        }
+
+        //fixedupdate called every fixed frame-rate frame
+        void FixedUpdate()
+        {
+            //vector 2 which is x y velocity
+            //equals to the velocity of the rigidbody2D
+            Vector2 veolocity = rb.velocity;
+            //velocity of x axis equals to
+            //the direction movement on the x axis
+            //of the charactor
+            velocity.x = movement;
+            //rigidbody2d velocity equals to
+            //velocity of the object
+            rb.velocity = velocity;
+        
+        
+        }
+
 
     }
 }
